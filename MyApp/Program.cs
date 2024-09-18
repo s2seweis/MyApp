65ca130da8b3cf111049c990c72b7fb5137b4674
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-09-18 09:12:26 +0200

    Projektdateien hinzufügen.

---
+++ MyApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string username = "Sebastian Weissenborn";
+
+            username = "Peter";
+
+            // create a variable
+
+            Console.WriteLine(username);
+
+            //Last assigned value is the current value
+
+            int age = 36;
+
+            age = 30;
+            
+            Console.WriteLine(age);
+
+            //Last assigned value is the current value
+
+            //Shortcuts:
+            // end of the line semicolon, for command is over
+            // begining of the line: shift + 7
+            // end of the line: shift + 1
+            // move line up and down: alt + up/down
+
+        }
+    }
+
+
+
+}
