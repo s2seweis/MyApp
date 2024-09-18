d6eb53cdca9f094973d0cec62ac2873677233e65
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-09-18 10:24:21 +0200

    add basic project structure, rename files

---
+++ MyApp/Variables.cs
@@ -0,0 +1,56 @@
+﻿namespace MyApp
+    //both in the same namespace: MyApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            FloatCalculator.CalculateFloats();
+
+            string username = "Sebastian Weissenborn";
+
+            //username = "Peter"; // Uncomment this line to change the username
+
+            // create a variable
+
+            Console.WriteLine(username);
+
+            // Last assigned value is the current value
+
+            // Declare Variables:
+            int age = 36;
+            age = 30;
+
+            Console.WriteLine(age);
+
+            // Last assigned value is the current value
+
+            // Shortcuts:
+            // end of the line semicolon, for command is over
+            // beginning of the line: shift + 7
+            // end of the line: shift + 1
+            // move line up and down: alt + up/down
+
+            // Calculate Numbers:
+            int a = 100;
+            int b = 200;
+
+            int result = a + b;
+
+            Console.WriteLine(result);
+
+            // Shortcuts:
+            // Ctrl + K, then Ctrl + C (this will comment out the selected block)
+            // Press Ctrl + K, then Ctrl + U (this will uncomment the selected block)
+
+            // Good to know:
+
+            // Console.ReadLine(); 
+            // Wartet auf eine Eingabe, damit die Konsole offen bleibt
+            // Zoom in/out: strg + mouse rad
+
+            Console.ReadKey(); // Wartet auf eine Taste, damit die Konsole offen bleibt
+        }
+    }
+}
