6ca0e04d15070ee67e8aa12a1f33a73e853d2a3d
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-09-19 09:18:23 +0200

    add user input

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,41 +1,63 @@
 ﻿namespace MyApp
-    //both in the same namespace: MyApp
+    // Namespace-Scope
 {
     internal class Program
+    // Class-Scope
     {
-        private static void Main(string[] args)
+
+        //Variable in class level/scope/ scope
+        static string username = "Hans";
+  
+        //private static void Main(string[] args)
+        //// Main method from which the program starts, scope limited to the class in which it was defined
+        //{
+        //    //username = "Peter";
+        //    //Console.WriteLine(username);
+
+        //    //Methods:
+        //    //FloatCalculator.CalculateFloats();
+        //    //StringCalculator.CalculateStrings();
+        //    //VariableCalculator.CalculateVariables();
+        //    //BoolCalculator.CalculateBool();
+        //    //CharCalculator.CalculateChar();
+
+        //    Console.ReadKey(); // Wartet auf eine Taste, damit die Konsole offen bleibt
+
+        //}
+
+        static void Main(string[] args)
         {
 
-            FloatCalculator.CalculateFloats();
-            StringCalculator.CalculateStrings();
-            VariableCalculator.CalculateVariables();
-            BoolCalculator.CalculateBool();
-            CharCalculator.CalculateChar();
+            Console.WriteLine("Bitte trage deinen namen ein:");
+            //e.g we call the Console Class and select one method
 
-            // Shortcuts:
-            // end of the line semicolon, for command is over
-            // beginning of the line: shift + 7
-            // end of the line: shift + 1
-            // move line up and down: alt + up/down
-            // copy line strg + e after strg + v
-            // delete line strg + x
-            // Ctrl + K, then Ctrl + C (this will comment out the selected block)
-            // Press Ctrl + K, then Ctrl + U (this will uncomment the selected block)
-            // mark words: alt + shift + . (repeatedly)
+            string username = "Sebastian";
+            string name = Console.ReadLine();
+            // return value string, when you slide over the method you see the return value string "strin?Console.readLine()"
 
-            // Good to know:
-            // Console.ReadLine(); 
-            // Wartet auf eine Eingabe, damit die Konsole offen bleibt
-            // Zoom in/out: strg + mouse rad
-
-            //Longer Comments:
-            /* 
-             * 
-             */
-
-
-            Console.ReadKey(); // Wartet auf eine Taste, damit die Konsole offen bleibt
-
+            Console.WriteLine("Dein Name ist also: " + name  );
         }
+
     }
 }
+
+// Shortcuts:
+// end of the line semicolon, for command is over
+// beginning of the line: shift + 7
+// end of the line: shift + 1
+// move line up and down: alt + up/down
+// copy line strg + e after strg + v
+// delete line strg + x
+// Ctrl + K, then Ctrl + C (this will comment out the selected block)
+// Press Ctrl + K, then Ctrl + U (this will uncomment the selected block)
+// mark words: alt + shift + . (repeatedly)
+
+// Good to know:
+// Console.ReadLine(); 
+// Wartet auf eine Eingabe, damit die Konsole offen bleibt
+// Zoom in/out: strg + mouse rad
+
+//Longer Comments:
+/* 
+ * 
+ */
